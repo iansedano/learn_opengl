2fa4741072c44ae3ac41f256f7390e2c1040e98d
iansedano <iansedano@gmail.com>
2021-02-27 16:01:24 +0100

    coord systems 3d boxes

--- main.cpp
+++ main.cpp
@@ -47,17 +47,56 @@ int main()
 		return -1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	// Init shaders
 	Shader ourShader("shaders/6_1_coordsys.vs", "shaders/6_1_coordsys.fs");
 
 	// set up vertex data (and buffers) and config vertex attributes
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,	 1.0f, 1.0f, // top right
-		 0.5f, -0.5f, 0.0f,	 1.0f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f,	 0.0f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f,  0.0f, 1.0f, // top left
-	};
+		-0.5f, -0.5f, -0.5f,	 0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,	 1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,	 1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,	 1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,	 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	 0.0f, 0.0f,
 
+		-0.5f, -0.5f,  0.5f,	 0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,	 1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,	 1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,	 1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,	 0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,	 0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,	 1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,	 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	 0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,	 0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,	 1.0f, 0.0f,
+					   
+		 0.5f,  0.5f,  0.5f,	 1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,	 1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,	 0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,	 0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,	 0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,	 1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,	 0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,	 1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,	 1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,	 1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,	 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,	 0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,	 0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,	 1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,	 1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,	 1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,	 0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,	 0.0f, 1.0f
+
+	};
 	unsigned int indices[] = {
 		0, 1, 2, // first triangle
 		0, 2, 3  // second triangle
@@ -145,42 +184,108 @@ int main()
 	ourShader.use();
 	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
 	ourShader.setInt("texture2", 1);
+	
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f, -1.0f, 0.0f),
+		glm::vec3(2.0f, 5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f, 3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f, 2.0f, -2.5f),
+		glm::vec3(1.5f, 0.2f, -1.5f),
+		glm::vec3(-1.3f, 1.0f, -1.5f)
+	};
+
+	
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
+		
+
+		float pulsing = sin(glfwGetTime());
+		float pulsing2 = cos(glfwGetTime());
+		float rising = glfwGetTime();
 
 		// Rendering commands here
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		glm::mat4 transform = glm::mat4(1.0f); // identity matrix
-		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.5f, 0.6f, 1.0f));
-		transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, 0.5f));
+
+
+		// Init identity matrices
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+
+
+		model = glm::rotate(
+			model,
+			rising * glm::radians(50.0f),
+			glm::vec3(0.5f, 1.0f, 0.0f)
+		);
+		view = glm::translate(
+			view,
+			glm::vec3(0.0f, 1.0f, -5.0f)
+		);
+		projection = glm::perspective(
+			glm::radians(pulsing * 100.0f),
+			(float)SCR_WIDTH / (float)SCR_HEIGHT,
+			0.1f,
+			100.0f
+		);
+		unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+		unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+
+		ourShader.setMat4("projection", projection);
+
+
+		//glm::mat4 transform = glm::mat4(1.0f); // identity matrix
+		//transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.5f, 0.6f, 1.0f));
+		//transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, 0.5f));
 
 		ourShader.setFloat("mixValue", mixValue);
-		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+		//unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+		//glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-		transform = glm::mat4(1.0f);
-		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(1.0f, 0.2f, 0.5f));
-		transform = glm::translate(transform, glm::vec3(0.0f, 0.5f, 0.0f));
-		float scaleAmount = sin(glfwGetTime());
-		transform = glm::scale(transform, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
+		for (unsigned int i = 0; i < 10; ++i) {
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, pulsing * cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, rising * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			ourShader.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		//transform = glm::mat4(1.0f);
+		//transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(1.0f, 0.2f, 0.5f));
+		//transform = glm::translate(transform, glm::vec3(0.0f, 0.5f, 0.0f));
+		//float scaleAmount = sin(glfwGetTime());
+		//transform = glm::scale(transform, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
+
+		//glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Check and call events and swap buffers
+		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
